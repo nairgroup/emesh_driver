fa946f3054d2875751930bacaa56df7d77f2e4da
nairgroup <nairgroup@users.noreply.github.com>
2015-11-19 16:33:11 -0600

    Update emesh_driver.py
    removed places for am2315 sensor. We are not using this sensor
    commented out wind for now since we don't have the anemometer or wind vane
    added code for tipping rain gauge

--- emesh_driver.py
+++ emesh_driver.py
@@ -3,7 +3,6 @@ import time
 import Adafruit_BMP.BMP085 as BMP085 #For use with the BMP185 pressure sensor
 import RPi.GPIO as GPIO #For use with GPIO pins
 from sht1x.Sht1x import Sht1x as SHT1x #For use with the SHT1x series of temperature sensors
-#from AM2315 import AM2315 #For use with am2315 temperature sensor
 
 #Setting GPIO mode
 GPIO.setmode(GPIO.BCM)
@@ -11,25 +10,36 @@ GPIO.setmode(GPIO.BCM)
 #Defining save file name
 savefile = "metobs.txt"
 
+#Defining time for loop delay
+dt = 5 #seconds
+
 #Setting pins to be used with each instrument
 sht1x_datapin = 11 #GPIO number
 sht1x_clkpin = 7 #GPIO number
+rain_pin = 25 #GPIO number
 
-windspd_pin = 23
+
+#Defining interrupt function for the rain bucket
+def rain_detect():
+	rain_count = rain_count + 1
+	return
+GPIO.setup(rain_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(rain_pin, GPIO.FALLING, callback=rain_detect, bouncetime=500)
+
+#windspd_pin = 23
 #winddir_pin = 0 #This is an analog signal and needs to be run thorugh an A->D converter
 
-
 #Variable initialization
-windspd_count = 0
+#windspd_count = 0
 
 #Defining interrupts for wind measurements
-GPIO.setup(windspd_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(windspd_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def windspd_detection():
-	windspd_count = windspd_count + 1
-	return
-	
-GPIO.add_event_detect(windspd_pin, GPIO.FALLING, callback=windspd_detection)
+#def windspd_detection():
+#	windspd_count = windspd_count + 1
+#	return
+
+#GPIO.add_event_detect(windspd_pin, GPIO.FALLING, callback=windspd_detection)
 
 #Creating object for sht1x themperature sensor
 sht1x = SHT1x(sht1x_datapin, sht1x_clkpin, SHT1x.GPIO_BCM)
@@ -43,7 +53,7 @@ lun.close()
 
 #-----------------BEGIN MAIN LOOP------------------------
 while (1):
-	time.sleep(5) #5 second delay in the loop
+	time.sleep(dt) 
 	#Reading from the BMP sensor
 	bmp = BMP085.BMP085()
 	bmp_temp = bmp.read_temperature()
@@ -52,15 +62,17 @@ while (1):
 	print "bmp temp is: ", bmp_temp
 	print "bmp pres is: ", bmp_pres
 	
-	#Reading from the AM2315
-	
-	
 	#Reading from the SHT1x
 	sht1x_temp = sht1x.read_temperature_C()
 	sht1x_rh = sht1x.read_humidity()
 	
 	print "sht1x temp is: ", sht1x_temp
 	print "sht1x rh is: ", sht1x_rh
+	
+	#Determining rain rate
+	rain_rate = (rain_count*0.2/dt)*3600 #rain rate in mm/hr
+	
+	print "rain rate is: ", rain_rate
 	
 	#Write to the data file
 	lun = open(savefile, 'a')
