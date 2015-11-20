5e2f2ba4f2eb6603fa5879c18eb8522886fbfd23
nairgroup <nairgroup@users.noreply.github.com>
2015-11-20 14:48:33 -0600

    Update emesh_driver.py
    Begun making file writing operational.
    Begun adding time stamps to data points

--- emesh_driver.py
+++ emesh_driver.py
@@ -52,14 +52,25 @@ sht1x = SHT1x(sht1x_datapin, sht1x_clkpin, SHT1x.GPIO_BCM)
 
 #Creating file header
 lun = open(savefile, 'w')
-lun.write("Header")
+lun.write("Year", "\t", "Julian Day", "Hour", "\t", "Minute", "\t", "Second", "\t", 
+	"Temp1 (K)", "Temp2 (K)", "\t", "Pressure (hPa)", "\t", "RH (%)", "\t",
+	"WindDir (V)", "\t", "WindSpd", "\t", "Rain Rate (mm/hr)", "\t",
+	"Year", "\t", "Julian Day", "\t", "Hour", "\t", "Minute", "\t", "Second")
 lun.write("\n")
 lun.close()
 
 
 #-----------------BEGIN MAIN LOOP------------------------
 while (1):
-	time.sleep(dt) 
+	time.sleep(dt)
+	
+	#Time of sensor read start
+	year1 = time.strftime("%Y")
+	day1 = time.strftime("%j")
+	h1 = time.strftime("%H")
+	m1 = time.strftime("%M")
+	s1 = time.strftime("%S")
+	
 	#Reading from the BMP sensor
 	bmp = BMP085.BMP085()
 	bmp_temp = bmp.read_temperature()
@@ -75,6 +86,10 @@ while (1):
 	print "sht1x temp is: ", sht1x_temp
 	print "sht1x rh is: ", sht1x_rh
 	
+	#Determining Wind Speed and Wind Direction
+	wind_dir = float('NaN')
+	wind_spd = float('NaN')
+	
 	#Determining rain rate
 	rain_rate = (rain_count*0.2/dt)*3600 #rain rate in mm/hr
 	
@@ -83,9 +98,19 @@ while (1):
 	
 	rain_count = 0
 	
+	
+	#Time of sensor read end
+	year2 = time.strftime("%Y")
+	day2 = time.strftime("%j")
+	h2 = time.strftime("%H")
+	m2 = time.strftime("%M")
+	s2 = time.strftime("%S")
+	
+	
 	#Write to the data file
 	lun = open(savefile, 'a')
-	lun.write("I made a file!")
+	lun.write(year1, day1, h1, m1, s1, sht1x_temp, bmp_temp, bmp_pres, sht1x_rh,
+		wind_dir, wind_spd, rain_rate, year2, day2, h2, m2, s2))
 	lun.write("\n")
 	lun.close()
 	
