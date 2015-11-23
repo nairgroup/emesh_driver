eb8ead19689d3757dec966817e4015b504ff276a
nairgroup <nairgroup@users.noreply.github.com>
2015-11-23 17:54:29 -0600

    Update emesh_driver.py

--- emesh_driver.py
+++ emesh_driver.py
@@ -157,8 +157,8 @@ except KeyboardInterrupt:
 	print "Keyboard Interrupt, cleaning pins..."
 
 
-except:
-	print "Unexpected Error"
+#except:
+#	print "Unexpected Error"
 
 
 finally: GPIO.cleanup() #Exiting code cleanly
