c34dd89c276a3ce1b39b77ea3e441995e1840223
nairgroup <nairgroup@users.noreply.github.com>
2016-03-14 13:45:26 -0500

    Added files via upload
    This code allows for the R. Pi to communicate via I2C with an Arduino. The Arduino takes the actual measurements.

---
+++ piard_emesh_driver.py
@@ -0,0 +1,63 @@
+#This routine is to be used on a Raspberry Pi to
+#gather observational data from an Arduino via the I2C bus
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#List of I2C write values and what they return
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 1 - Returns Temperature from the BMP sensor
+# 2 - Returns Temperature from the SHT1X Sensor
+# 3 - Returns Relative Humidity from the SHT1X Sensor
+# 4 - Returns Pressure from the BMP Sensor
+# 5 - Returns Wind Direction from the Anemometer
+# 6 - Returns Wind Speed from the Anemometer
+# 7 - Returns Rain from the Tipping Rain Gauge
+# 8 - Returns the Time from the Arduino's Clock Chip
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#Importing necessary modules
+import numpy
+import smbus
+import time
+import RPi.GPIO as GPIO
+
+#Defining GPIO parameters
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Defining I2C parameters
+arduino_address = 0x04
+
+#I2C functions
+def write_signal(obsv)
+	bus.write_byte(arduino_address, obsv)
+	return -1
+	
+def read_signal()
+	value = bus.read_byte(address)
+	return value
+
+#Getting observations
+def pull_obs(obsv)
+	write_signal(obsv)
+	nels = read_signal()
+	data = numpy.zeros(nels)
+	for i in range(nels):
+		data[i] = read_signal()
+		
+	return data
+
+print(data)
+	
+#Telling R. Pi to wait for Arduino's signal before pulling obs
+try:
+	GPIO.wait_for_edge(7, GPIO.RISING)
+	bmptmp_bytes = pull_obs(1)
+	print(bmptmp_bytes.T)
+	
+	#Write observations to files
+	
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	
+GPIO.cleanup
